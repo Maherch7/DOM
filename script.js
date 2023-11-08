56f235695da0370bfa40ef772d9da9e4b9a05ac6
Maherch7 <146967082+Maherch7@users.noreply.github.com>
2023-11-08 14:46:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var btnP = document.querySelectorAll('.btnP')
 for(let i = 0;i<btnP.length;i++){
     btnP[i].addEventListener('click',function(){
         btnP[i].previousElementSibling.innerText++
+        SommeTotal()
     })
 }
 
@@ -14,6 +15,7 @@ for(let i = 0;i<btnM.length;i++){
     btnM[i].addEventListener('click',function(){
         if (btnM[i].nextElementSibling.innerText>0)
         btnM[i].nextElementSibling.innerText--
+        SommeTotal()
     })
 }
 
@@ -22,6 +24,7 @@ var btnT = document.querySelectorAll('.fa-trash')
 for(let i = 0;i<btnT.length;i++) {
     btnT[i].addEventListener('click', function(){
         btnT[i].parentElement.remove()
+        SommeTotal()
     })
 }
 
